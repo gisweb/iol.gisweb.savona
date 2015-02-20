38b969999afa19dff48cb5d986356e81fa12b765
mamogmx <marco.carbone@gmx.com>
2015-02-20 05:49:47 +0100

    Adde terreni e urbano in ws praticaweb

--- iol/gisweb/savona/browser/views.py
+++ iol/gisweb/savona/browser/views.py
@@ -5,7 +5,7 @@ from plone import api
 from zope.component import getUtility
 from iol.gisweb.savona.IolApp import IolApp
 from iol.gisweb.savona.IolPraticaWeb import IolWSPraticaWeb
-
+import random
 
 class inviaPW(object):
 
@@ -15,7 +15,7 @@ class inviaPW(object):
 
     def __call__(self):
         doc = self.aq_parent
-        url = "http://webservice.gisweb.it/wspraticaweb/savona.wsPraticaweb.php?wsdl&test=4"
+        url = "http://webservice.gisweb.it/wspraticaweb/savona.wsPraticaweb.php?wsdl&test=%d" %random.randint(1,100000)
         wsDoc = IolWSPraticaWeb(doc,url)
         res = wsDoc.aggiungi_pratica()
         return res
